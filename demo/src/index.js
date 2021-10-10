8db6ea245e42847b421678de37fbc841ca3f85da
樊淼军 <595779487@qq.com>
2021-10-10 14:54:01 +0800

    1

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/home.scss';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- demo/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function(app) {
-    app.use(
-      '/home',createProxyMiddleware({
-        target: 'https://home-api.pinduoduo.com/',
-        changeOrigin: true,
-      })
-    );
-};
